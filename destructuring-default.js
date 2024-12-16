ccf4d436f5aa47c417ba6e989bd577cf96f80cad
Ramkumar <ramkumar87.bca@gmail.com>
2024-12-16 18:10:12 +0530

    changes on destructuring default  on dev branch

--- destructuring-default.js
+++ destructuring-default.js
@@ -1,3 +1,4 @@
+//With destructuring we can set defaults, or fallback values so that if an item is not in the object (or Array, Map, or Set) it will fall back to what you have set at the default.
 const userDets = { name: "Ram" };
 const { name, age = 35 } = userDets;
 console.log(name);
